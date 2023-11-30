0b397d7f945d2061cda6582b4482be009fc9742d
AlexeyQQQ <leadterrier@gmail.com>
2023-11-30 20:55:53 +0300

    Domain: added use cases and repository

---
+++ app/src/main/java/com/example/shoppinglist/domain/DeleteShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.shoppinglist.domain
+
+class DeleteShopItemUseCase(private val shopItemRepository: ShopItemRepository) {
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        shopItemRepository.deleteShopItem(shopItem)
+    }
+}
