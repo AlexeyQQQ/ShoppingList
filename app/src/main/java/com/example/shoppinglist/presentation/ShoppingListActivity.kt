cb4126747037ce119e8176cba5b74557b2f6e4fa
AlexeyQQQ <leadterrier@gmail.com>
2023-11-30 18:18:15 +0300

    Creating project

---
+++ app/src/main/java/com/example/shoppinglist/presentation/ShoppingListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.shoppinglist.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shoppinglist.R
+
+class ShoppingListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping_list)
+    }
+}
